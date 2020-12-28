d481bdda49cff1023fe10848f0ae75ed539d98cb
Your Name <gagatunmd@gmail.com>
2020-12-28 18:32:37 +0200

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const http = require('http');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8000;
 const list = [];
+let a=1 ;
 
 const server = http.createServer((req, res) => {
     switch (req.url) {
@@ -13,13 +14,13 @@ const server = http.createServer((req, res) => {
             res.setHeader('Charset', 'UTF-8');
             res.end(fs.readFileSync('index.html'), 'UTF-8', null);
             break;
-        case '/todo-list':
+        case '/message':
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/json');
             res.setHeader('Charset', 'UTF-8');
             res.end(JSON.stringify(list));
             break;
-        case '/add-todo':
+        case '/add':
             if(req.method != 'POST')
             {
                 res.statusCode = 400;
@@ -29,14 +30,16 @@ const server = http.createServer((req, res) => {
             req.on('data', (chunk) => {
                 
                 let obj = JSON.parse(chunk.toString('UTF-8'));
-                if(!obj && !obj.text && !obj.completed)
+                if(!obj && !obj.text )
                 {
                     res.statusCode = 400;
                     res.end();
                     return;
                 }
-                list.unshift(obj);
+                list.unshift({nomer:a,text:obj.text});
+                a++;
                 list.splice(5);
+                
                 res.statusCode = 202;
                 res.end();
             });
